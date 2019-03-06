ebc1aa4b0aa9c715157c1196732b35a3f90c277b
Stefan <stefankollaart@gmail.com>
2019-03-06 11:04:26 +0100

    initial

---
+++ main.rb
@@ -0,0 +1,18 @@
+my_hash = {
+  email: "stefankollaart@gmail.com",
+  leeftijd: 23,
+  woonplaats: "Hoofddorp",
+  job: "Docent/programmeur"
+}
+
+puts my_hash[:naam]
+
+my_hash[:naam] = "Stefan Kollaart"
+
+puts my_hash[:naam]
+
+puts my_hash[:leeftijd]
+
+my_hash[:leeftijd] = 24
+
+puts my_hash[:leeftijd]
